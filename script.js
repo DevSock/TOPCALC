e956797e61cf10d744ae0145ec5b07127cdf3c28
DevSock <mikeashley227@yahoo.com>
2022-06-15 19:24:08 -0400

    Update display values when operands are clicked

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 const calculator = {
   displayValue: "0",
   firstOperand: null,
-  secondOperand: null,
-  waitingForSecondOperand: false,
   operator: null,
+  secondOperand: null,
 };
 
-const preOperatorElement = document.querySelector(".pre-operator");
-const postOperatorElement = document.querySelector(".post-operator");
+const topDisplayElement = document.querySelector(".top-display");
+const bottomDisplayElement = document.querySelector(".bottom-display");
+const operators = document.querySelectorAll(".operator");
+const operands = document.querySelectorAll(".operand");
 
 const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
@@ -16,6 +17,30 @@ const divide = (a, b) => a / b;
 
 function setDisplayText(displayElement, operand) {
   displayElement.textContent = operand;
+}
+
+function handleOperandClick(event) {
+  const targetTextContent = event.target.textContent;
+
+  if (!calculator.firstOperand) {
+    calculator.firstOperand = +targetTextContent;
+    setDisplayText(bottomDisplayElement, calculator.firstOperand);
+    return;
+  } else if (!calculator.operator) {
+    calculator.firstOperand = +`${calculator.firstOperand}${targetTextContent}`;
+    setDisplayText(bottomDisplayElement, calculator.firstOperand);
+    return;
+  } else if (!calculator.secondOperand) {
+    calculator.secondOperand = +targetTextContent;
+    setDisplayText(topDisplayElement, calculator.firstOperand);
+    setDisplayText(bottomDisplayElement, calculator.secondOperand);
+    return;
+  } else {
+    calculator.secondOperand =
+      +`${calculator.secondOperand}${targetTextContent}`;
+    setDisplayText(bottomDisplayElement, calculator.secondOperand);
+    return;
+  }
 }
 
 function operate(operator, a, b) {
@@ -32,3 +57,7 @@ function operate(operator, a, b) {
       return "Error";
   }
 }
+
+operands.forEach((element) => {
+  element.addEventListener("click", handleOperandClick);
+});
